e70def87a67f2a214de2e33334c1d465f5ad131f
Mark Lester Dampios <mlesterdampios@gmail.com>
2020-05-05 09:15:28 +0800

    Update link.service.ts

--- src/app/link.service.ts
+++ src/app/link.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class LinkService {
 
   apiLink: string = "https://cc106.syncshop.online/";
-  consumer_key: string  = "ck_xxx";
-  consumer_secret: string  = "ck_xxx";
+  consumer_key: string  = "ck_33b119c8a8fad0361062e227bad14291972c5c5b";
+  consumer_secret: string  = "cs_540f38435d134b672c0d5807497977d33503e5f5";
   constructor() { }
 
   getAPILink() : string{
