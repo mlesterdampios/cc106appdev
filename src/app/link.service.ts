bd45cb28f76fd97fb634ef8bef0301e2d6ed7461
Mark Lester Dampios <mlesterdampios@gmail.com>
2020-10-06 07:18:29 +0800

    update

--- src/app/link.service.ts
+++ src/app/link.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class LinkService {
 
   apiLink: string = "https://cc106.rxmsolutions.com/";
-  consumer_key: string  = "ck_9317a7ab7b5faf18664be6de904434b988c2209b";
-  consumer_secret: string  = "cs_55f0248def4c1bce9cda1f68df7fef01bc4a6f3d";
+  consumer_key: string  = "ck_xxxx";
+  consumer_secret: string  = "cs_xxxx";
   constructor() { }
 
   getAPILink() : string{
